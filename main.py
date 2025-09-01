b3bce069f64dd4c950a85033e73aa3631da8235f
IvanSSantana <ivansantospriv@gmail.com>
2025-08-31 23:37:47 -0300

    Implementing get, put and args with type, help and required

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {
-   "ivan": {
-   "age": 17,
-   "gender": "male"
-},
-   "mary": {
-   "age": 21,
-   "gender": "female"
-   }
-}
+# Create the request object
+video_put_args = reqparse.RequestParser()
 
-class HelloWorld(Resource):
-   def get(self, name):
-      return names[name]
+# Add the arguments for request above
+video_put_args.add_argument("likes", type=int, help="Number of video likes")
+video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
+video_put_args.add_argument("views", type=int, help="Number of video views")
+
+videos = {}
+
+class Video(Resource):
+   def get(self, video_id: int):
+      return videos[video_id]
    
-   def post(self):
-      return {"data": "Posted"}
+   def put(self, video_id: int):
+      # Parse to request args according to the indicated variable
+      args = video_put_args.parse_args()
+      videos[video_id] = args
+      return {video_id: args}, 201
 
-api.add_resource(HelloWorld, "/helloworld/<string:name>")
+api.add_resource(Video, "/video/<int:video_id>")
 
 if __name__ == "__main__":
    app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld/ivan")
+response = requests.put(BASE + "video/1", json={"likes": 10, "name": "First Video", "views": 100})
+print(response.json())
+
+response = requests.get(BASE + "video/1")
 print(response.json())
