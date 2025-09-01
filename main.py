afe86898c311f5ee43e10442bb4ebf9f20308b13
IvanSSantana <ivansantospriv@gmail.com>
2025-08-31 22:51:04 -0300

    First steps in APIs with Flask

--- main.py
+++ main.py
@@ -4,5 +4,25 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {
+   "ivan": {
+   "age": 17,
+   "gender": "male"
+},
+   "mary": {
+   "age": 21,
+   "gender": "female"
+   }
+}
+
+class HelloWorld(Resource):
+   def get(self, name):
+      return names[name]
+   
+   def post(self):
+      return {"data": "Posted"}
+
+api.add_resource(HelloWorld, "/helloworld/<string:name>")
+
 if __name__ == "__main__":
    app.run(debug=True)
